522159f3b70e8d402a07ac8dde657a7e765e48f7
kaniameliana <100584085+kaniameliana@users.noreply.github.com>
2022-10-20 20:30:14 +0700

    Add files via upload

---
+++ Nomor 2/Nomor2 interface.php
@@ -0,0 +1,14 @@
+<!-- Kania Meliana Fityanti_21091397028 -->
+     
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
